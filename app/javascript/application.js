12178a631bb69a8d958ca522ef2daf867f45a819
thisguyEly <thisguyely@gmail.com>
2024-09-07 11:03:43 -0400

    added favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+//= require semantic-ui
+//= require rails-ujs
+//= require turbolinks
+//= require_tree.
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery
-//= require semantic-ui
+import '@doabit/semantic-ui-sass'
+import $ from 'jquery'
+window.$ = $
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown')
+  .dropdown();
+})

57f6c0cf9c46a82ec4386c0328c4f7b3c6dd7a19
thisguyEly <thisguyely@gmail.com>
2024-09-09 14:15:28 -0400

    cleanup & navigation back to chatroom page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,7 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-//= require semantic-ui
-//= require rails-ujs
 //= require turbolinks
-//= require_tree.
-import "@hotwired/turbo-rails"
-import "controllers"
-import '@doabit/semantic-ui-sass'
-import $ from 'jquery'
-window.$ = $
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+//= require semantic-ui
+//= require_tree .
 
 $(document).on('turbolinks:load', function() {
-  $('.ui.dropdown')
-  .dropdown();
+  $('.ui.dropdown').dropdown();
 })
